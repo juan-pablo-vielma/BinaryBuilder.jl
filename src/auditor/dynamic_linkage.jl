1bb246d62df5d15cf3286db6a92a8d4fef739ea9
Juan Pablo Vielma <jvielma@mit.edu>
2018-09-01 18:14:42 -0400

    Update dynamic_linkage.jl

--- src/auditor/dynamic_linkage.jl
+++ src/auditor/dynamic_linkage.jl
@@ -278,7 +278,8 @@ function update_linkage(prefix::Prefix, platform::Platform, path::AbstractString
     # libraries that this guy is interested in have been `dlopen()`'ed previously,
     # (and have the appropriate SONAME) things should "just work".
     logpath = joinpath(logdir(prefix), "update_linkage_$(basename(path))_$(basename(old_libpath)).log")
-    new_libpath = relpath(new_libpath, dirname(path))
+    #new_libpath = relpath(new_libpath, dirname(path))
+    new_libpath = basename(new_libpath)
     if Compat.Sys.isapple(platform)
         # On MacOS, we need to explicitly add `@rpath/` before our library linkage path.
         new_libpath = joinpath("@rpath", new_libpath)
